9dc0f20c6dd6fd64ed367077f391dcf0a591539c
Sreshtha Mehrotra <sreshtha.mehrotra@gmail.com>
2025-05-27 01:02:37 +0530

    setup basic api structure

---
+++ api/api.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, HTTPException
+import dotenv
+import os
+from pydantic import BaseModel
+
+dotenv.load_dotenv()
+
+fi_base_url = os.getenv("FI_BASE_URL")
+re_base_url = os.getenv("RE_BASE_URL")
+
+app = FastAPI()
+
+class RequestedFix(BaseModel):
+    branch:str
+    github_token:str
+
+class RequestedReview(BaseModel):
+    pr_url:str
+    github_token:str    
+
+class RequestedFIRE(BaseModel):
+    branch:str
+    github_token:str
+    scrutiny: int
+
+@app.post('/fix')
+async def fix(data:RequestedFix):
+    pass
+
+@app.post('/review')
+async def review(data:RequestedReview):
+    pass
+
+@app.post('/fire')
+async def fire(data:RequestedFIRE):
+    pass
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="0.0.0.0",port="8080")
