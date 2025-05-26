c40c43fb780ce49e6f3ce179e6878b9074ed3cac
Sreshtha Mehrotra <sreshtha.mehrotra@gmail.com>
2025-05-27 01:52:53 +0530

    init - github.py

--- scripts/github.py
+++ scripts/github.py
@@ -1,1 +1,15 @@
 #script for github functions.
+import requests
+import os
+from dotenv import load_dotenv
+from git import Repo
+import shutil
+from datetime import datetime
+import uuid
+
+
+def copy_file_contents(file_url, github_token):
+    pass
+
+def create_pr(pr_url, github_token):
+    pass
